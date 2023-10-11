bf000964edc103470924391911ab9c8a392bebce
Kbuyukkol <kamuran.buyukkol@gmail.com>
2023-10-11 13:41:21 +0300

    first commit

---
+++ 01_Variables.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+
+// single comment
+
+/**
+ * Multiple comment line
+ */
+
+let a;
+console.log(typeof a); //undefined
+
+a= 0;
+
+console.log(typeof a); //number
+
+a= 'javascript';
+
+console.log(typeof a); //string
+
+let x=5, y= 'Ali';
+
+console.log(x+y); //concatination
+
+console.log(x*y); //NaN
+
---
+++ 02_Strings.js
@@ -0,0 +1,15 @@
+let str1= "Java";
+
+let str2 = 'Script';
+
+let str3= "awesome";
+
+let result;
+
+result= str1+str2+' '+str3;
+
+console.log(result);
+
+result= `${str1}${str2} ${str3}` // with backticks
+
+console.log(result);
