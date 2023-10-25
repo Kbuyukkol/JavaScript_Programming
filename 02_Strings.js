69536cb22033a70da8d20c802dd3ceb8c591bef4
Kbuyukkol <kamuran.buyukkol@gmail.com>
2023-10-25 13:31:09 +0300

    strings methods added

--- 02_Strings.js
+++ 02_Strings.js
@@ -13,3 +13,42 @@ console.log(result);
 result= `${str1}${str2} ${str3}` // with backticks
 
 console.log(result);
+
+//String methods
+
+let word= "JavaScript Programming";
+
+//length
+console.log(word.length); 
+
+console.log(word[0]); //first character
+
+console.log(word[word.length-1]); //last character
+
+//substring 
+console.log(word.substring(0,10)); 
+
+//startsWith
+console.log(word.startsWith('J')); //true
+console.log(word.startsWith('j')); // false
+
+//endsWith
+console.log(word.endsWith('g')); //true
+
+//indexOf
+console.log(word.indexOf('a')); //1 : first occurence
+
+//slice
+console.log(word.slice(0,4)); //Java
+
+//toUpperCase
+console.log(word.toUpperCase()); //JAVASCRIPT PROGRAMMING
+
+//toLowerCase
+console.log(word.toLowerCase()); //javascript programming
+
+//replace
+console.log(word.replace('Script','')); //Java Programming
+
+
+
