781a0d2de4f49e1a4db6efa3b44e8db537cd9b11
Kbuyukkol <kamuran.buyukkol@gmail.com>
2023-10-25 15:37:31 +0300

    practice tasks added

---
+++ practiceTasks/finra.js
@@ -0,0 +1,16 @@
+for(let i=1; i<=100;i++){
+    if (i % 15 == 0) {
+        console.log("FINRA");
+    }
+
+    else if (i % 3 == 0) {
+        console.log("FIN");
+    }
+    else if (i % 5 == 0) {
+        console.log("RA");
+    }
+    else{
+        console.log(i);
+    }
+}
+        
